0996526564ae57f1ee4b80f7c231d02452df2960
Phani54321 <92142841+Phani54321@users.noreply.github.com>
2021-12-14 19:44:01 +0530

    m

---
+++ c.js
@@ -0,0 +1,46 @@
+if ( 9=='9'){
+    console.log(true);
+}else {
+    console.log(false);
+}
+
+if ( 9===8){
+    console.log(true);
+}else {
+    console.log(false);
+}
+
+if ( 9!='8'){
+    console.log(true);
+}else {
+    console.log(false);
+}
+
+if ( 9!=='9'){
+    console.log(true);
+}else {
+    console.log(false);
+}
+if ( 9>'9'){
+    console.log(true);
+}else {
+    console.log(false);
+}
+
+if ( 9<'9'){
+    console.log(true);
+}else {
+    console.log(false);
+}
+
+if ( 9<='9'){
+    console.log(true);
+}else {
+    console.log(false);
+}
+
+if ( 9>='9'){
+    console.log(true);
+}else {
+    console.log(false);
+}
